cef0eb2839b5cdb100b92e4dc1dc74945d2c23d4
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 16:09:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name).to_i + 1} in line."
 end
 
 def now_serving(katz_deli)
