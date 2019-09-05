99ae0bb4b059f385adf4f418539241be914befa4
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 16:19:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
       katz_deli.each_with_index do |name,index|
       shoppers << "#{index + 1}. #{name}"
     end
-    puts "The line is currently:" + shoppers.join(" ")
+    puts "The line is currently: " + shoppers.join(" ")
   end
 end
 
