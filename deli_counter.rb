a3eb0afff7c7386823cd06cc5a5890779f698f68
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 16:13:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: #{katz_deli.each do |name|
+    puts "The line is currently:" + katz_deli.each do |name|
     (katz_deli.index(name).to_i + 1).to_s + ". " + name
-      end}"
+      end
   end
 end
 
