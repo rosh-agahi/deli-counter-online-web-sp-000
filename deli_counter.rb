3c829f8456f2304556cc2c29566803b36785c6a6
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 16:00:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = [] 
+
+def line(katz_deli)
+  if katz_deli.size == 0 
+    return "The line is currently empty."
+  else 
+    return print_line(katz_deli)
+  end
+end
+
+def take_a_number(katz_deli,name)
+  katz_deli << name
+  "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+end
+
+def print_line(katz_deli)
+  "The line is currently:" + katz_deli.each do |name|
+    (katz_deli.index(name).to_i + 1).to_s + ". " + name
+  end
+end
+
+def now_serving(katz_deli)
+  "Currently serving #{katz_deli[0]}."
+  katz_deli.shift
+end
